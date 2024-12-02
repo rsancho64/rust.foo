2ba2f07b3c1f18f8ed25e0d7253b6115e47bcb67
rsancho64 <rsancho@iesrioarba.es>
2024-12-02 11:04:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+matando ranas con rust
---
+++ escalaChar.rs
@@ -0,0 +1,13 @@
+// fn aux(){
+//     print!("esto desde la aux...");
+// }
+
+// This is the main function.
+fn main() {
+
+    let mychar = 'f';
+    let posicion = mychar.to_digit(10);
+    print!("letra: {} en posicion {}", mychar, posicion);
+
+}
+
